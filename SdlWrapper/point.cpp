892d02dc31f9c4792ef3c079b2cc6001651a20af
tyr-sl3 <vincent.penando@hotmail.fr>
2017-01-13 16:04:11 +0100

    Update

--- SdlWrapper/point.cpp
+++
@@ -1,8 +0,0 @@
-#include "point.h"
-
-sdl::Point::operator SDL_Point() const noexcept{
-  SDL_Point point;
-  point.x = x;
-  point.y = y;
-  return point;
-}
--- SdlWrapper/sprite.h
+++
@@ -1,6 +0,0 @@
-#ifndef __SPRITE_H__
-#define __SPRITE_H__
-
-
-
-#endif // __SPRITE_H__
