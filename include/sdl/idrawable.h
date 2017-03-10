d7bdf07eae4a3aec3c4ce03477cf25151a3260bb
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2017-03-10 22:54:17 +0100

    Create idrawable.h

---
+++ include/sdl/idrawable.h
@@ -0,0 +1,37 @@
+#ifndef __IDRAWABLE_H__
+#define __IDRAWABLE_H__
+
+// **********************
+// ** Standard library **
+//***********************
+// -
+
+// **********************
+// ** SDL header files **
+// **********************
+// -
+
+// **********************
+// ** Custom lib files **
+// **********************
+#include <sdl/api/nullable.h> // sdl::api::Nullable
+#include <sdl/point.h>        // sdl::Point
+#include <sdl/rect.h>         // sdl::Rect
+
+namespace sdl {
+
+  class IDrawable {
+    friend class Window;
+    using NullableRect = api::Nullable<Rect>;
+  public:
+    IDrawable() = default;
+    virtual ~IDrawable() = default;
+
+  private:
+    // Will be called in Window::blit(IDrawable const&)
+    virtual void on_window(Window const& window, Point const& coords, NullableRect const& src_rect = api::NULL_VAL) const = 0;
+  };
+
+} // namespace sdl
+
+#endif // __IDRAWABLE_H__
