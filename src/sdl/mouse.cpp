88ef87fd0318ec82798a6d401562de0f6c79c741
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2017-03-17 14:57:33 +0100

    Update mouse.cpp

--- src/sdl/mouse.cpp
+++ src/sdl/mouse.cpp
@@ -5,42 +5,29 @@ sdl::MouseState::MouseState(Point const& coords, Uint32 state) noexcept
 {
 }
 
-sdl::Point sdl::MouseState::coords() const noexcept{
+sdl::Point sdl::MouseState::coords() const noexcept {
   return coords_;
 }
 
-bool sdl::MouseState::clicked(sdl::ClickCode c) const noexcept{
+bool sdl::MouseState::clicked(sdl::ClickCode c) const noexcept {
   return (state_ & c) != 0;
 }
 
-//sdl::mouse::MouseState sdl::mouse::state(bool read_events) noexcept{
-//  int x;
-//  int y;
-//  if(read_events){
-//    SDL_PumpEvents();
-//  }
-//  const auto ret = SDL_GetMouseState(&x, &y);
-//  const sdl::Point coords{x, y};
-//  return sdl::mouse::MouseState{coords, ret};
-//}
-
 sdl::MouseStateHandler::MouseStateHandler(bool read_events)
   : read_events_{read_events},
-  state_{sdl::Point{0, 0}, 0}{
-
+  state_{sdl::Point{0, 0}, 0}
+{
 }
 
-void sdl::MouseStateHandler::update(){
-  int x;
-  int y;
-  if(read_events_){
+void sdl::MouseStateHandler::update() {
+  if (read_events_) {
     SDL_PumpEvents();
   }
-  const auto ret = SDL_GetMouseState(&x, &y);
-  const sdl::Point coords{x, y};
+  sdl::Point coords{};
+  const auto ret = SDL_GetMouseState(&coords.x, &coords.y);
   state_ = sdl::MouseState{coords, ret};
 }
 
-sdl::MouseState sdl::MouseStateHandler::state() const{
+sdl::MouseState sdl::MouseStateHandler::state() const {
   return state_;
 }
