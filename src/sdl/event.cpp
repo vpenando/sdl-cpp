39305668e59f4919bcef00460ad629b2700e2646
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2017-03-17 14:59:43 +0100

    Update event.cpp

--- src/sdl/event.cpp
+++ src/sdl/event.cpp
@@ -7,34 +7,38 @@ sdl::EventHandler::EventHandler()
 {
 }
 
-void sdl::EventHandler::on_quit(Action const& fun){
+void sdl::EventHandler::on_quit(Action const& fun) {
   assert(!on_quit_defined_ && "Already defined");
   on_quit_ = fun;
   on_quit_defined_ = true;
 }
 
-void sdl::EventHandler::update(){
-  if(!on_quit_defined_){
+void sdl::EventHandler::update() {
+  if (!on_quit_defined_) {
     throw std::runtime_error{"You must bind an exit function by calling EventHandler::on_quit before using it"};
   }
   sdl::Event e;
-  while(SDL_PollEvent(&e)){
-    if(e.type == SDL_QUIT){
+  while (SDL_PollEvent(&e)) {
+    if (e.type == SDL_QUIT) {
       on_quit_();
     }
   }
   keyboard_handler_.update();
   mouse_handler_.update();
-  if(actions_.size() > 0){
-    const auto keyboard_state = keyboard_handler_.state();
-    for(auto& pair : actions_){
-      if(keyboard_state[pair.first].pressed){
-        pair.second();
-      }
-    }
+  if (actions_.size() > 0) {
+    handle_actions();
   }
 }
 
-void sdl::EventHandler::on_press(sdl::KeyCode code, Action const& fun){
+void sdl::EventHandler::on_press(sdl::KeyCode code, Action const& fun) {
   actions_.insert(std::pair<KeyCode, Action>(code, fun));
+}
+
+void sdl::EventHandler::handle_actions() {
+  const auto keyboard_state = keyboard_handler_.state();
+  for (auto& pair : actions_) {
+    if (keyboard_state[pair.first].pressed) {
+      pair.second();
+    }
+  }
 }
