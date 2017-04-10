3bde64b77ae40e750fbcf78fd98a3696894bfedb
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2017-04-10 14:05:06 +0200

    Create types.h

---
+++ include/sdl/types.h
@@ -0,0 +1,27 @@
+#ifndef __TYPES_H__
+#define __TYPES_H__
+
+// **********************
+// ** Standard library **
+//***********************
+// -
+
+// **********************
+// ** SDL header files **
+// **********************
+#include <SDL.h> // Uint8, Uint16, Uint32
+
+// **********************
+// ** Custom lib files **
+// **********************
+// -
+
+namespace sdl {
+  
+  using u8 = Uint8;
+  using u16 = Uint16;
+  using u32 = Uint32;
+  
+} // namespace sdl
+
+#endif // __TYPES_H__
