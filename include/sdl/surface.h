1c55ce08184719e2e8363aedd28f946ff41634a1
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2017-04-18 16:38:17 +0200

    Update surface.h

--- include/sdl/surface.h
+++ include/sdl/surface.h
@@ -29,7 +29,7 @@ namespace sdl{
     Surface(Surface const& surface);
     Surface& operator=(Surface const& surface);
     Size size() const noexcept;
-    operator SDL_Surface*();
+    operator SDL_Surface*() const noexcept;
   private:
     void copy_surface(Surface const& surface);
     SDL_Surface *ptr_;
