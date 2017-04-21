4bf08d87996378975e76e4e722c1e5206698d4bc
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2017-04-21 15:16:05 +0200

    Update rect.h

--- include/sdl/rect.h
+++ include/sdl/rect.h
@@ -19,15 +19,32 @@
 #include <sdl/size.h>             // sdl::Size
 #include <sdl/point.h>            // sdl::Point
 
-namespace sdl{
+//! @namespace sdl
+//! @brief The SDL wrapper main namespace
+namespace sdl {
 
+  //! @class Rect
+  //! @brief The SDL_Rect equivalent
+  //! @extends IConvertible
+  //! @extends Component
   struct Rect final :
     public api::IConvertible<SDL_Rect>,
     public ecs::Component
   {
+    //! @brief Default ctor
     Rect() noexcept;
+    //! @brief Ctor
+    //! @param width   The rectangle width
+    //! @param height  The rectangle height
     Rect(unsigned width, unsigned height) noexcept;
-    Rect(int coordX, int coordY, unsigned width, unsigned height) noexcept;
+    //! @brief Ctor
+    //! @param coord_x The X coordinate
+    //! @param coord_y The Y coordinate
+    //! @param width   The rectangle width
+    //! @param height  The rectangle height
+    Rect(int coord_x, int coord_y, unsigned width, unsigned height) noexcept;
+    //! @brief Ctor
+    //! @param rect A SDL_Rect
     Rect(SDL_Rect const& rect) noexcept;
     operator SDL_Rect() const noexcept override;
     int x, y;
