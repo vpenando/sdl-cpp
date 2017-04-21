3210089f1400e41b61162ec5d14ea8bf910cc622
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2017-04-21 09:59:06 +0200

    Changed sdl::Point

--- include/sdl/point.h
+++ include/sdl/point.h
@@ -4,7 +4,7 @@
 // **********************
 // ** Standard library **
 //***********************
-#include <type_traits> // std::is_arithmetic
+// -
 
 // **********************
 // ** SDL header files **
@@ -18,39 +18,27 @@
 #include <sdl/ecs/ecs.h>          // sdl::ecs::Component
 
 namespace sdl{
-  namespace api{
-
-    template<class T>
-    struct Vector2{
-      static_assert(std::is_arithmetic<T>::value, "Invalid type");
-      Vector2() noexcept : x(T{}), y(T{}){}
-      Vector2(T p_x, T p_y) noexcept : x(p_x), y(p_y){}
-      T x, y;
-    };
-
-    using Vector2i = Vector2<int>;
-    using Vector2f = Vector2<float>;
-  } // namespace sdl::api
 
   struct Point final :
-    public api::Vector2i,
+    private SDL_Point,
     public api::IConvertible<SDL_Point>,
     public ecs::Component
   {
-    using api::Vector2i::Vector2i;
+    Point() noexcept : Point(0, 0) {}
+    Point(int p_x, int p_y) noexcept : SDL_Point(p_x, p_y) {}
+    using SDL_Point::x;
+    using SDL_Point::y;
     operator SDL_Point() const noexcept override;
   };
 
 } // namespace sdl
 
-template<class T>
-inline bool operator==(sdl::api::Vector2<T> const& v1, sdl::api::Vector2<T> const& v2) noexcept{
-  return v1.x == v2.x && v1.y == v2.y;
+inline bool operator==(sdl::Point const& p1, sdl::Point const& p2) noexcept{
+  return p1.x == p2.x && p1.y == p2.y;
 }
 
-template<class T>
-inline bool operator!=(sdl::api::Vector2<T> const& v1, sdl::api::Vector2<T> const& v2) noexcept{
-  return !(v1 == v2);
+inline bool operator!=(sdl::Point const& p1, sdl::Point const& p2) noexcept{
+  return !(p1 == p2);
 }
 
 #endif // __VECTOR2_H__
