f2b327d2de2b683c9833ae0e71c8fdbdde22ac43
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2017-05-22 17:19:27 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ int main(int, char **){
     // Event handler
     sdl::EventHandler handler;
     handler.on_quit([&loop](){ loop = false; });
-    handler.on_press(sdl::KeyCode::A, [](){
+    handler.on<sdl::keyboard::Press::A>([](){
       std::cout << "Key A pressed" << std::endl;
     });
     auto& mouse_handler = handler.get<sdl::MouseStateHandler>();
