b098384ccadee84ae2e462035b56028a2c86efb1
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2017-06-25 16:03:48 +0200

    Update exception.cpp

--- src/sdl/exception.cpp
+++ src/sdl/exception.cpp
@@ -3,6 +3,6 @@
 sdl::Exception::Exception(std::string const& what) : what_{what} {
 }
 
-const char *sdl::Exception::what() const {
+const char *sdl::Exception::what() const noexcept {
   return what_.c_str();
 }
